120c8b5c421b3164aa52d876c5e52571b9e84b9c
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-11-09 19:08:35 +0900

    Title: 길이 단위 변환하기, Time: 168ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241109/길이 단위 변환하기/change-length-unit.js
@@ -0,0 +1,8 @@
+const FEET = 30.48;
+const MILE = 160934
+
+const ft = 9.2;
+const mi = 1.3;
+
+console.log(`${ft}ft = ${(ft * FEET).toFixed(1)}cm`);
+console.log(`${mi}mi = ${(mi * MILE).toFixed(1)}cm`);
