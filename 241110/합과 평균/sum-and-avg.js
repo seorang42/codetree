a6745088579b671e60974a5e49dddabd9a5b6337
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-11-10 17:37:03 +0900

    Title: 합과 평균, Time: 178ms, Memory: 14MB, Status: Passed - CodeTree

--- 241110/합과 평균/sum-and-avg.js
+++ 241110/합과 평균/sum-and-avg.js
@@ -1,4 +1,4 @@
 const fs = require("fs");
 const [a, b] = fs.readFileSync(0).toString().trim().split(" ").map(el => Number(el));
 
-console.log([a + b, (a + b) / 2].join(" "));
+console.log([a + b, ((a + b) / 2).toFixed(1)].join(" "));
