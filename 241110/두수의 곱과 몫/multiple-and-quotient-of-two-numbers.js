486b74ff217c4b3f0ff60afd83f40c87fc28d998
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-11-10 17:20:58 +0900

    Title: 두수의 곱과 몫, Time: 179ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241110/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+const [a, b] = fs.readFileSync(0).toString().trim().split(" ").map(el => Number(el));
+
+console.log(`${a} * ${b} = ${a * b}`);
+console.log(`${a} / ${b} = ${a / b}`);
