3f31aed264985587b095419eca54c109e8107f80
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-11-10 18:28:50 +0900

    Title: 최대 2번의 연산, Time: 184ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241110/최대 2번의 연산/up-to-2-calculations.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+let a = Number(fs.readFileSync(0).toString().trim());
+
+if (a % 2 === 0) {
+    a /= 2;
+}
+if (a % 2 !== 0) {
+    (a + 1) / 2;
+}
+
+console.log(a.toFixed(0));
