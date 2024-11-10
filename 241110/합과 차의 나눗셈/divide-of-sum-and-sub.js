f3d516a960a4910c737fa823974175041e49984d
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-11-10 17:34:00 +0900

    Title: 합과 차의 나눗셈, Time: 174ms, Memory: 14MB, Status: Passed - CodeTree

--- 241110/합과 차의 나눗셈/divide-of-sum-and-sub.js
+++ 241110/합과 차의 나눗셈/divide-of-sum-and-sub.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const [a, b] = fs.readFileSync(0).toString().trim().split(" ");
+const [a, b] = fs.readFileSync(0).toString().trim().split(" ").map(el => Number(el));
 
 const result = ((a + b) / (a - b)).toFixed(2);
 console.log(result);
