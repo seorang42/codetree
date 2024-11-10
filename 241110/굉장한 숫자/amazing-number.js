5aa98af0d985a723337262c866d88aae1a0543c3
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-11-10 19:31:01 +0900

    Title: 굉장한 숫자, Time: 174ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241110/굉장한 숫자/amazing-number.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+const a = Number(fs.readFileSync(0).toString().trim());
+
+const result = (a % 2 !== 0 || a % 3 === 0) && (a % 2 === 0 || a % 5 === 0);
+console.log(result);
