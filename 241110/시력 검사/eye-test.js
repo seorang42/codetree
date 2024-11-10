0ad1d888662e68b27c9e649e58f33722d3368977
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-11-10 19:07:30 +0900

    Title: 시력 검사, Time: 166ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241110/시력 검사/eye-test.js
@@ -0,0 +1,4 @@
+const fs = require("fs");
+const [right, left] = fs.readFileSync(0).toString().trim().split("\n").map(el => Number(el));
+
+console.log(right >= 1 && left > 1 ? "High" : right >= 0.5 && left >= 0.5 ? "Middle" : "Low");
