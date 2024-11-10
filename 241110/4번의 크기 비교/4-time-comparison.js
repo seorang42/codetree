e87cdef6cc1b63c599df4b0911ba26ff4a8c4b0a
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-11-10 18:57:52 +0900

    Title: 4번의 크기 비교, Time: 164ms, Memory: 14MB, Status: Runtime Error - CodeTree

--- 241110/4번의 크기 비교/4-time-comparison.js
+++ 241110/4번의 크기 비교/4-time-comparison.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 let [a, bcde] = fs.readFileSync(0).toString().trim().split("\n");
 a = Number(a);
-bcde = bcde.map(el => Number(el));
+bcde = bcde.split(" ").map(el => Number(el));
 
 const array = [a > b, a > c, a > d, a > e];
 array.forEach(el => console.log(el ? 1 : 0));
