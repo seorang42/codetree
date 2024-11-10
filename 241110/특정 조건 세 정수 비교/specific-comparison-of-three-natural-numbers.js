0379bf16c205fc4683cedfecb7a72ee5e7ef8ee5
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-11-10 19:18:11 +0900

    Title: 특정 조건 세 정수 비교, Time: 183ms, Memory: 14MB, Status: Passed - CodeTree

--- 241110/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
+++ 241110/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
@@ -1,4 +1,4 @@
 const fs = require("fs");
 const [a, b, c] = fs.readFileSync(0).toString().trim().split(" ").map(el => Number(el));
 
-console.log(`${Math.min(a, b, c) === a} ${a === b && b === c}`);
+console.log(`${Math.min(a, b, c) === a ? 1 : 0} ${a === b && b === c ? 1 : 0}`);
