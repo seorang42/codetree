455728d69d7eeec07a717e4c6ae6994f8ae64a40
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-11-11 11:33:59 +0900

    Title: 369 게임, Time: 177ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241111/369 게임/369-game.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const n = Number(fs.readFileSync(0).toString().trim());
+
+const result = [];
+for (let i = 1; i <= n; i++) {
+    result.push(i % 3 === 0 || String(i).includes(3) === -1 ? 0 : i);
+}
+
+console.log(result.join(" "));
