e9dd9134a3fa06a55a22b13429362e85dfa86939
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-11-11 09:49:14 +0900

    Title: 짝수만 출력 2, Time: 186ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241111/짝수만 출력 2/output-only-even-2.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+let [b, a] = fs.readFileSync(0).toString().trim().split(el => Number(el));
+
+const result = [];
+while(b >= a) {
+    if (b % 2 === 0) {
+        result.push(b);
+        b -= 2;
+    } else {
+        b--;
+    }
+}
+
+console.log(result.join(" "));
