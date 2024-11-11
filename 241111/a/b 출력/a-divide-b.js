0f224dbc3d5c7e5b20e5c71527a6ac025d0df56f
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-11-11 10:28:25 +0900

    Title: a/b 출력, Time: 173ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 241111/a/b 출력/a-divide-b.js
+++ 241111/a/b 출력/a-divide-b.js
@@ -1,18 +1,11 @@
 const fs = require("fs");
 let [a, b] = fs.readFileSync(0).toString().trim().split(" ").map(el => Number(el));
 
-let result = "0.";
+let result = `${Math.floor(a / b)}.`;
 for (let i = 0; i < 20; i++) {
-    if (b !== 0) {
-        if (a < b) a *= 10;
-        let [tempA, tempB] = [a, b];
-        tempA = Math.floor(a / b);
-        result += tempA;
-        tempB = a % b;
-        [a, b] = [tempA, tempB];
-    } else {
-        result += 0;
-    }
+    a *= 10;
+    result += Math.floor(a / b);
+    a %= b;
 }
 
 console.log(result);
