a8efb1c23541f5066dd7b6d94d58ffd089c2b63f
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-11-11 11:26:58 +0900

    Title: a 부터 b 까지, Time: 188ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 241111/a 부터 b 까지/a-to-b.js
+++ 241111/a 부터 b 까지/a-to-b.js
@@ -1,14 +1,15 @@
 const fs = require("fs");
 let [a, b] = fs.readFileSync(0).toString().trim().split(" ").map(el => Number(el));
 
-const result = [a];
+const result = [];
 while (a < b) {
+    result.push(a);
     if (a % 2 !== 0) {
         a *= 2;
     } else {
         a += 3;
     }
-    result.push(a);
 }
+result.push(a);
 
 console.log(result.join(" "));
