c1eb3a8b60835f784ead74440219db3d58539e64
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-11-11 10:33:55 +0900

    Title: a/b 출력, Time: 169ms, Memory: 14MB, Status: Passed - CodeTree

--- 241111/a/b 출력/a-divide-b.js
+++ 241111/a/b 출력/a-divide-b.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 let [a, b] = fs.readFileSync(0).toString().trim().split(" ").map(el => Number(el));
 
 let result = `${Math.floor(a / b)}.`;
+a %= b;
 for (let i = 0; i < 20; i++) {
     a *= 10;
     result += Math.floor(a / b);
