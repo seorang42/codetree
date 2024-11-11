5bfc9cf8bf1e0f66edbc48076d3e5ec6f2711f81
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-11-11 09:38:54 +0900

    Title: 3의 배수 출력하기, Time: 5087ms, Memory: 14MB, Status: Time Limit Exceed - CodeTree

---
+++ 241111/3의 배수 출력하기/print-multiple-of-number-three.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const n = Number(fs.readFileSync(0).toString().trim());
+
+const result = [];
+let index = 1;
+while (index <= n) {
+    if (index % 3 === 0) {
+        result.push(index);
+    }
+}
+
+console.log(result.join(" "));
