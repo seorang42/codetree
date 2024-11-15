422f6f32016fcaff9e21b7664ca16ecab033232a
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-11-16 00:22:54 +0900

    Title: 문자열 범위 출력하기 2, Time: 177ms, Memory: 15MB, Status: Passed - CodeTree

--- 241116/문자열 범위 출력하기 2/print-string-in-range-2.js
+++ 241116/문자열 범위 출력하기 2/print-string-in-range-2.js
@@ -2,8 +2,14 @@ const fs = require("fs");
 const [str, length] = fs.readFileSync(0).toString().trim().split("\n");
 
 let answer = "";
-for (let i = str.length - 1; i > str.length - 1 - length; i--) {
-    answer += str[i];
+if (str.length >= length) {
+    for (let i = str.length - 1; i > str.length - 1 - length; i--) {
+        answer += str[i];
+    }
+} else {
+    for (let i = str.length - 1; i >= 0; i--) {
+        answer += str[i];
+    }
 }
 
 console.log(answer);
