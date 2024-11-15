4be442370e0f98cdb9a6fb1745cde72fe24b845f
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-11-16 01:21:28 +0900

    Title: 문자열 놀이, Time: 173ms, Memory: 15MB, Status: Passed - CodeTree

--- 241116/문자열 놀이/play-with-string.js
+++ 241116/문자열 놀이/play-with-string.js
@@ -1,16 +1,17 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split("\n");
-const [s, q] = input[0].split(" ");
+let [s, q] = input[0].split(" ");
 
 for (let i = 1; i <= Number(q); i++) {
     const query = input[i].split(" ");
     const array = s.split("");
+    let result = "";
     if (Number(query[0]) === 1) {
         const [a, b] = [Number(query[1]), Number(query[2])];
         const temp = s[a - 1];
         array[a - 1] = array[b - 1];
         array[b - 1] = temp;
-        console.log(array.join(""));
+        result = array.join("");
     } else if (Number(query[0]) === 2) {
         const [a, b] = [query[1], query[2]];
         array.forEach((el, index) => {
@@ -18,6 +19,8 @@ for (let i = 1; i <= Number(q); i++) {
                 array[index] = b;
             }
         });
-        console.log(array.join(""))
+        result = array.join("");
     }
+    console.log(result);
+    s = result;
 }
