2295f75af8b9ea8c0eb3bf63fde92538e8e13fd0
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-11-16 03:06:09 +0900

    Title: 일치하는 문자열의 수, Time: 175ms, Memory: 15MB, Status: Passed - CodeTree

--- 241116/일치하는 문자열의 수/num-of-correct-string.js
+++ 241116/일치하는 문자열의 수/num-of-correct-string.js
@@ -3,7 +3,7 @@ const input = fs.readFileSync(0).toString().trim().split("\n");
 const [n, A] = input[0].split(" ");
 
 let count = 0;
-for (let i = 1; i <= Number(n); i++) {
+for (let i = 1; i <= n; i++) {
     if (input[i] === A) {
         count++;
     }
