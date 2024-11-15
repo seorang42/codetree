5a8604c441984f306ff8ab1fe23459a5199f202c
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-11-16 00:18:25 +0900

    Title: 문자열에서 문자 출력, Time: 169ms, Memory: 15MB, Status: Passed - CodeTree

--- 241116/문자열에서 문자 출력/print-chars-from-word.js
+++ 241116/문자열에서 문자 출력/print-chars-from-word.js
@@ -1,3 +1,5 @@
 const fs = require("fs");
-const answer = fs.readFileSync(0).toString().trim().split("").join("\n");
-console.log(answer);
+const answer = fs.readFileSync(0).toString().trim();
+for (let i = 0; i < answer.length; i++) {
+    console.log(answer[i]);
+}
