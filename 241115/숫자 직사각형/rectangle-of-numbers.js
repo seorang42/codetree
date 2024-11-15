eb742b1e60880fa5f6ab8b25a6acba568dcc9c82
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-11-15 20:27:41 +0900

    Title: 숫자 직사각형, Time: 189ms, Memory: 15MB, Status: Passed - CodeTree

--- 241115/숫자 직사각형/rectangle-of-numbers.js
+++ 241115/숫자 직사각형/rectangle-of-numbers.js
@@ -2,14 +2,12 @@ const fs = require("fs");
 const [height, width] = fs.readFileSync(0).toString().trim().split(" ").map(el => Number(el));
 
 let count = 1;
-const answer = [];
-for (let i = 1; i <= height; i++) {
-    const line = [];
-    for (let j = 1; j <= width; j++) {
-        line.push(count);
+const answer = Array(height).fill(0).map(el => Array(width).fill(0));
+for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+        answer[i][j] = count;
         count++;
     }
-    answer.push(line.join(" "));
 }
 
-console.log(answer.join("\n"));
+answer.forEach(el => console.log(el.join(" ")));
