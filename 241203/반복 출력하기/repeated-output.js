2ec60fac99a11811b4e09c2d5d92eb1d0b5cfbb6
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-12-03 22:30:32 +0900

    Title: 반복 출력하기, Time: 188ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241203/반복 출력하기/repeated-output.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = Number(fs.readFileSync(0).toString().trim());
+
+const print = (n) => {
+    for (let i = 0; i < n; i++) {
+        console.log("12345^&*()_");
+    }
+}
+
+print(input);
