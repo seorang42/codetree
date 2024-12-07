ae43152435df9c08c9d7da04a30ac07340bd9837
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-12-07 21:17:47 +0900

    Title: 최소공배수 구하기, Time: 178ms, Memory: 15MB, Status: Passed - CodeTree

--- 241207/최소공배수 구하기/find-the-least-common-multiple.js
+++ 241207/최소공배수 구하기/find-the-least-common-multiple.js
@@ -1,19 +1,13 @@
 const fs = require("fs");
 const [a, b] = fs.readFileSync(0).toString().trim().split(" ").map(el => Number(el));
 
-const calc = (a, b) => {
-    let [num1, num2] = [a, b];
-    let [count1, count2] = [1, 1];
-    while (num1 !== num2) {
-        if (num1 > num2) {
-            count1++;
-            num2 = a * count1;
-        } else {
-            count2++;
-            num1 = b * count2;
-        }
+const gcd = (x, y) => {
+    while (y !== 0) {
+        [x, y] = [y, x % y];
     }
-    console.log(num1);
+    return x;
 }
 
-calc(a, b);
+const lcm = (x, y) => x * y / gcd(x, y);
+
+console.log(lcm(a, b));
