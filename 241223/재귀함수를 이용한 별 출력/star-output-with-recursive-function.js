b34427f8153c6006db943097831a2cd4c015ba00
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-12-23 19:09:07 +0900

    Title: 재귀함수를 이용한 별 출력, Time: 185ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241223/재귀함수를 이용한 별 출력/star-output-with-recursive-function.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const input = Number(fs.readFileSync(0).toString().trim());
+
+const printStar = (n) => {
+    if (n === 0) {
+        return;
+    }
+
+    printStar(n - 1);
+
+    let line = "";
+    for (let i = 1; i <= n; i++) {
+        line += "*";
+    }
+    console.log(line);
+}
+
+printStar(input);
