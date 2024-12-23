3964dd6731a930c7e05453a3011e84779c8ed362
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-12-23 17:31:13 +0900

    Title: 2021년 날짜의 유무, Time: 176ms, Memory: 15MB, Status: Passed - CodeTree

--- 241223/2021년 날짜의 유무/with-or-without-2021.js
+++ 241223/2021년 날짜의 유무/with-or-without-2021.js
@@ -3,7 +3,7 @@ const [M, D] = fs.readFileSync(0).toString().trim().split(" ").map(el => Number(
 
 const days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 const checkDay = (m, d) => {
-    return days[m - 1] > d ? "Yes" : "No";
+    return days[m - 1] >= d ? "Yes" : "No";
 }
 
 console.log(checkDay(M, D));
