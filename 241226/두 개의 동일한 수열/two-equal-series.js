7d968e7faa93e9f5a824e94dc93c795dc77062fc
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-12-26 17:09:14 +0900

    Title: 두 개의 동일한 수열, Time: 158ms, Memory: 15MB, Status: Passed - CodeTree

--- 241226/두 개의 동일한 수열/two-equal-series.js
+++ 241226/두 개의 동일한 수열/two-equal-series.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split("\n");
-const [arr1, arr2] = [input[1].split(" ").map(el => Number(el)).sort((a, b) => a - b), input[2].split(" ").map(el => Number(el)).sort((a, b) => a - b)];
+const [arr1, arr2] = [input[1].trim().split(" ").map(el => Number(el)).sort((a, b) => a - b), input[2].trim().split(" ").map(el => Number(el)).sort((a, b) => a - b)];
 
 console.log(JSON.stringify(arr1) === JSON.stringify(arr2) ? "Yes" : "No");
