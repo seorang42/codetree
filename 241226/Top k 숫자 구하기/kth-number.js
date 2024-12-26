5a68d4d22297dfce4af89a9c5ad33f94bbcbcb5c
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-12-26 16:56:48 +0900

    Title: Top k 숫자 구하기, Time: 171ms, Memory: 15MB, Status: Passed - CodeTree

--- 241226/Top k 숫자 구하기/kth-number.js
+++ 241226/Top k 숫자 구하기/kth-number.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split("\n");
 const [N, k] = input[0].split(" ").map(el => Number(el));
-const arr = input[1].split(" ").map(el => Number(el)).sort();
+const arr = input[1].split(" ").map(el => Number(el)).sort((a, b) => a - b);
 
 console.log(arr[k - 1]);
