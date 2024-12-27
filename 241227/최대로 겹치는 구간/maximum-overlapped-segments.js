34b0832581b95f772ce6641d281b2b78f474b68e
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-12-27 18:08:57 +0900

    Title: 최대로 겹치는 구간, Time: 176ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241227/최대로 겹치는 구간/maximum-overlapped-segments.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const n = Number(input[0]);
+const arr = input.slice(1).map(el => el.split(" ").map(el => Number(el)));
+
+const line = Array(100).fill(0);
+arr.forEach(el => {
+    for (let i = el[0]; i < el[1]; i++) {
+        line[i]++;
+    }
+})
+
+console.log(line.sort((a, b) => b - a)[0]);
