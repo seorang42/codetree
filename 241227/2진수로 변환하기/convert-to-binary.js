c18e386308c83ffce368409fa1f60c7a8a91892f
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-12-27 17:46:34 +0900

    Title: 2진수로 변환하기, Time: 162ms, Memory: 15MB, Status: Passed - CodeTree

--- 241227/2진수로 변환하기/convert-to-binary.js
+++ 241227/2진수로 변환하기/convert-to-binary.js
@@ -5,7 +5,7 @@ const getBinNum = (n) => {
     if (n < 2) {
         return n;
     }
-    return String(n % 2) + getBinNum(parseInt(n / 2))
+    return getBinNum(parseInt(n / 2)) + String(n % 2)
 }
 
-console.log([...getBinNum(n)].reverse().join(""));
+console.log(getBinNum(n));
