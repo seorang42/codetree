557149f47cb9995f72d72d03c21241ae00f10dda
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-12-27 16:45:31 +0900

    Title: DateTime to DateTime, Time: 197ms, Memory: 15MB, Status: Passed - CodeTree

--- 241227/DateTime to DateTime/datetime-to-datetime.js
+++ 241227/DateTime to DateTime/datetime-to-datetime.js
@@ -5,4 +5,4 @@ const start = new Date(2011, 11, 11, 11, 11).getTime();
 const end = new Date(2011, 11, d, h, m).getTime();
 
 const answer = parseInt((end - start) / 1000 / 60);
-console.log(answer > 0 ? answer : -1);
+console.log(answer >= 0 ? answer : -1);
