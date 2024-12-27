f412c0005ce2c4f2434b5a99e9000cce421fa602
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-12-27 17:05:56 +0900

    Title: 그 요일은, Time: 186ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241227/그 요일은/the-day-of-the-day.js
+++ 241227/그 요일은/the-day-of-the-day.js
@@ -5,7 +5,7 @@ const target = input[1];
 const DAYS = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 const targetIndex = DAYS.indexOf(target);
 
-const [start, end] = [new Date(2024, m1, d1).getTime(), new Date(2024, m2, d2).getTime()];
+const [start, end] = [new Date(2024, m1 - 1, d1).getTime(), new Date(2024, m2 - 1, d2).getTime()];
 const diff = (end - start) / (1000 * 60 * 60 * 24);
 const answer = Math.ceil((diff - targetIndex) / 7);
 console.log(answer);
