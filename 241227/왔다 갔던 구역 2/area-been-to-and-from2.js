c97a8e598a5f243d0a7bda1e94ef984f43280166
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-12-27 18:36:24 +0900

    Title: 왔다 갔던 구역 2, Time: 179ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241227/왔다 갔던 구역 2/area-been-to-and-from2.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const n = Number(input[0]);
+const arr = input.slice(1).map(el => el.split(" "));
+const areaArr = [];
+let [current, highest, offset] = [0, 0, 0]; 
+arr.forEach(el => {
+    if (el[1] === "R") {
+        areaArr.push([current, current + Number(el[0])]);
+        current += Number(el[0]);
+        if (current > highest) {
+            highest = current;
+        }
+    } else {
+        areaArr.push([current, current - Number(el[0])]);
+        current -= Number(el[0]);
+        if (current < offset) {
+            offset = Math.abs(current);
+        }
+    }
+});
+
+const line = Array(highest + offset + 1).fill(0);
+areaArr.forEach(el => {
+    const [start, end] = el.sort((a, b) => a - b).map(el => el + offset);
+    for (let i = start; i < end; i++) {
+        line[i]++;
+    }
+});
+
+console.log(line.filter(el => el >= 2).length);
