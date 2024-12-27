b663f6f0d36940f526829c9481e0c352a7e24d82
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-12-27 16:44:34 +0900

    Title: DateTime to DateTime, Time: 191ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241227/DateTime to DateTime/datetime-to-datetime.js
+++ 241227/DateTime to DateTime/datetime-to-datetime.js
@@ -4,4 +4,5 @@ const [d, h, m] = fs.readFileSync(0).toString().trim().split(" ").map(el => Numb
 const start = new Date(2011, 11, 11, 11, 11).getTime();
 const end = new Date(2011, 11, d, h, m).getTime();
 
-console.log(parseInt((end - start) / 1000 / 60));
+const answer = parseInt((end - start) / 1000 / 60);
+console.log(answer > 0 ? answer : -1);
