dcf4d3a6efca9e6b3d95cf5e3c96e0d6b29f1c38
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-12-27 16:59:04 +0900

    Title: 요일 맞추기, Time: 196ms, Memory: 15MB, Status: Passed - CodeTree

--- 241227/요일 맞추기/guess-day-of-week.js
+++ 241227/요일 맞추기/guess-day-of-week.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const [m1, d1, m2, d2] = fs.readFileSync(0).toString().trim().split(" ").map(el => Number(el));
 const DAYS = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
-const [start, end] = [new Date(2011, m1, d1).getTime(), new Date(2011, m2, d2).getTime()];
+const [start, end] = [new Date(2011, m1 - 1, d1).getTime(), new Date(2011, m2 - 1, d2).getTime()];
 const diff = (end - start) / (1000 * 60 * 60 * 24);
 const remain = diff >= 0 ? diff % 7 : 7 - Math.abs(diff % 7);
 
