e88e166deddf3e8ebb033c0075979eddc1537092
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-12-28 21:24:41 +0900

    Title: 흰검 칠하기, Time: 204ms, Memory: 20MB, Status: Passed - CodeTree

--- 241228/흰검 칠하기/painting-white-black.js
+++ 241228/흰검 칠하기/painting-white-black.js
@@ -1,45 +1,56 @@
 const fs = require("fs");
-const input = fs.readFileSync(0).toString().trim().split("\n");
-const n = Number(input[0]);
-const commands = input.slice(1).map(el => el.split(" "));
+const input = fs.readFileSync(0).toString().trim().split('\n');
 
-class Move {
-    constructor (start, end, direction) {
-        this.start = start;
-        this.end = end;
-        this.direction = direction;
+const MAX_K = 100000;
+// 변수 선언 및 입력
+let n = Number(input[0]);
+let a = Array(2 * MAX_K + 1).fill(0);
+let cntB = Array(2 * MAX_K + 1).fill(0);
+let cntW = Array(2 * MAX_K + 1).fill(0);
+let b = 0, w = 0, g = 0;
+
+let cur = MAX_K;
+for (let i = 1; i <= n; i++) {
+    let [x, c] = input[i].split(' ');
+    x = Number(x);
+
+    if (c === 'L') {
+        // x칸 왼쪽으로 칠합니다.
+        while (x > 0) {
+            a[cur] = 1;
+            cntW[cur] += 1;
+            x -= 1;
+
+            if (x) {
+                cur -= 1;
+            }
+        }
+    } else {
+        // x칸 오른쪽으로 칠합니다.
+        while (x > 0) {
+            a[cur] = 2;
+            cntB[cur] += 1;
+            x -= 1;
+
+            if (x) {
+                cur += 1;
+            }
+        }
     }
 }
 
-const moveArr = [];
-let [current, offset, highest] = [0, 0, 0];
-commands.forEach(el => {
-    const [amount, direction] = [Number(el[0]), el[1]];
-    moveArr.push(new Move(current, direction === "R" ? current + amount - 1 : current - amount + 1, direction));
-    current = direction === "R" ? current + amount - 1 : current - amount + 1;
-    if (current > highest) highest = current;
-    if (current < offset) offset = current;
-});
-
-const line = Array(highest - offset + 1).fill(0).map(() => []);
-moveArr.forEach(el => {
-    const [start, end] = el.direction === "R" ? [el.start - offset, el.end - offset] : [el.end - offset, el.start - offset];
-    for (let i = start; i <= end; i++) {
-        if (line[i].includes("G")) continue;
-
-        if (el.direction === "R") {
-            if (line[i].filter(el => el === "B").length >= 2) continue;
-            line[i].push("B");
-        } else if (el.direction === "L") {
-            if (line[i].filter(el => el === "W").length >= 2) continue;
-            line[i].push("W");
-        }
-        
-        if (line[i].length === 4) {
-            line[i] = ["G"]
-        }
+for (let i = 0; i < 2 * MAX_K + 1; i++) {
+    // 검은색과 흰색으로 두 번 이상 칠해진 타일은 회색입니다.
+    if (cntB[i] >= 2 && cntW[i] >= 2) {
+        g += 1;
     }
-});
+    // 그렇지 않으면 현재 칠해진 색깔이 곧 타일의 색깔입니다.
+    else if (a[i] === 1) {
+        w += 1;
+    } else if (a[i] === 2) {
+        b += 1;
+    }
+}
 
-const [w, b, g] = [line.filter(el => el[el.length - 1] === "W").length, line.filter(el => el[el.length - 1] === "B").length, line.filter(el => el[el.length - 1] === "G").length];
-console.log(`${w} ${b} ${g}`)
+// 정답을 출력합니다.
+console.log(`${w} ${b} ${g}`);
