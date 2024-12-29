ec9d2d35e336dbfd602f514d33ed91ab198a5034
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-12-29 18:01:43 +0900

    Title: 선두를 지켜라 3, Time: 226ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 241229/선두를 지켜라 3/keep-the-lead-3.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const [N, M] = input[0].split(" ").map(el => Number(el));
+
+const A = input.slice(1, N + 1).map(el => el.split(" ").map(el => Number(el)));
+const B = input.slice(N + 1).map(el => el.split(" ").map(el => Number(el)));
+const [newA, newB] = [[], []];
+A.forEach(el => {
+    for (let i = 0; i < el[1]; i++) {
+        newA.push(el[0]);
+    }
+})
+B.forEach(el => {
+    for (let i = 0; i < el[1]; i++) {
+        newB.push(el[0]);
+    }
+})
+
+let [a, b] = [0, 0];
+let [count, farther] = [0, ""];
+for (let i = 0; i < newA.length; i++) {
+    a += newA[i];
+    b += newB[i];
+    const currentFarther = a > b ? "A" : a < b ? "B" : "AB";
+    if (farther === "") {
+        farther = currentFarther;
+        count++;
+    } else if (farther !== currentFarther) {
+        farther = currentFarther;
+        count++;
+    }
+}
+
+console.log(count);
