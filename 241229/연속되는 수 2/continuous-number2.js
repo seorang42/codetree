07bd0ba8a84e2fe94d87245ab444e4d63cb41da0
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-12-29 15:41:26 +0900

    Title: 연속되는 수 2, Time: 182ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241229/연속되는 수 2/continuous-number2.js
+++ 241229/연속되는 수 2/continuous-number2.js
@@ -3,11 +3,17 @@ const input = fs.readFileSync(0).toString().trim().split("\n");
 const n = Number(input[0]);
 const arr = input.slice(1).map(el => Number(el));
 
+let count = 0;
 let answer = 0;
 for (let i = 0; i < n; i++) {
     if (arr[i] !== arr[i - 1] || i === 0) {
-        answer++;
+        count++;
+    } else {
+        if (count > answer) {
+            answer = count;
+        }
     }
+    if (i === n - 1 && count > answer) answer = count;
 }
 
 console.log(answer);
