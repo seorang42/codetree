8344a96ae348151c2cdfcb0290d1a1deb2b4ba74
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-12-30 22:06:58 +0900

    Title: 격자 위의 편안한 상태, Time: 289ms, Memory: 21MB, Status: Passed - CodeTree

--- 241230/격자 위의 편안한 상태/comfortable-state-on-the-grid.js
+++ 241230/격자 위의 편안한 상태/comfortable-state-on-the-grid.js
@@ -4,13 +4,13 @@ const [N, M] = input[0].split(" ").map(el => Number(el));
 const arr = input.slice(1).map(el => el.split(" ").map(el => Number(el)));
 
 const getValue = (x, y) => {
-    return x < M && x >= 0 && y < M && y >= 0 ? board[y][x] : 0;
+    return x < N && x >= 0 && y < N && y >= 0 ? board[x][y] : 0;
 }
 
 const board = Array(N).fill(0).map(() => Array(N).fill(0));
 for (let i = 0; i < M; i++) {
     const [x, y] = arr[i].map(el => el - 1);
-    board[y][x] = 1;
+    board[x][y] = 1;
     const answer = getValue(x - 1, y) + getValue(x + 1, y) + getValue(x, y - 1) + getValue(x, y + 1) === 3 ? 1 : 0;
     console.log(answer);
 }
