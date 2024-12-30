64f69e5892c9fd34b53d0f6c53b3837fe66c8a59
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-12-30 21:22:15 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 181ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241230/빙빙 돌며 숫자 사각형 채우기/snail-number-square.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const [n, m] = fs.readFileSync(0).toString().trim().split(" ").map(el => Number(el));
+
+const arr = Array(n).fill(0).map(() => Array(m).fill(0));
+let dir = 0;
+const dx = [0, 1, 0, -1];
+const dy = [1, 0, -1, 0];
+
+const inRange = (x, y) => {
+    return x >= 0 && x < n && y >= 0 && y < m ? true : false;
+}
+
+let [x, y] = [0, 0];
+let count = 1;
+for (let i = 0; i < n * m; i++) {
+    if (i === 0) {
+        arr[x][y] = count;
+        count++;
+        continue;
+    }
+    if (!inRange(x + dx[dir], y + dy[dir]) || arr[x + dx[dir]][y + dy[dir]] !== 0) {
+        dir = (dir + 1) % 4;
+    }
+    [x, y] = [x + dx[dir], y + dy[dir]];
+    arr[x][y] = count;
+    count++;
+}
+
+console.log(arr);
