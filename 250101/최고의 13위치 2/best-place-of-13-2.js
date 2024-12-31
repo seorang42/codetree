0aadd39b3f715d545b51acd199248102219c6165
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2025-01-01 00:44:47 +0900

    Title: 최고의 13위치 2, Time: 162ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250101/최고의 13위치 2/best-place-of-13-2.js
+++ 250101/최고의 13위치 2/best-place-of-13-2.js
@@ -4,13 +4,20 @@ const N = Number(input[0]);
 const arr = input.slice(1).map(el => el.split(" ").map(el => Number(el)));
 
 let answer = 0;
-for (let i = 0; i < N - 1; i++) {
-    for (let k = 0; k < N - 2; k++) {
-        for (let j = i + 1; j < N; j++) {
-            for (let l = 0; l < N - 2; l++) {
-                const sum = arr[i][k] + arr[i][k + 1] + arr[i][k + 2] + arr[j][k] + arr[j][k + 1] + arr[j][k + 2];
-                if (sum > answer) {
-                    answer = sum;
+for (let i = 0; i < N; i++) {
+    if (N >= 6) {
+        for (let k = 0; k < N - 5; k++) {
+            for (let l = k + 3; l < N; l++) {
+                const sum = arr[i][k] + arr[i][k + 1] + arr[i][k + 2] + arr[i][l] + arr[i][l + 1] + arr[i][l + 2];
+                if (sum > answer) answer = sum;
+            }
+        }
+    } else if (i < N - 1) {
+        for (let k = 0; k < N - 2; k++) {
+            for (let j = i + 1; j < N; j++) {
+                for (let l = 0; l < N - 2; l++) {
+                    const sum = arr[i][k] + arr[i][k + 1] + arr[i][k + 2] + arr[j][k] + arr[j][k + 1] + arr[j][k + 2];
+                    if (sum > answer) answer = sum;
                 }
             }
         }
