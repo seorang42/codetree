88e0c16e6695b0b17e260261a907c5d428c07727
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-12-31 16:56:25 +0900

    Title: 모이자, Time: 199ms, Memory: 15MB, Status: Passed - CodeTree

--- 241231/모이자/gather.js
+++ 241231/모이자/gather.js
@@ -3,7 +3,7 @@ const input = fs.readFileSync(0).toString().trim().split("\n");
 const n = Number(input[0]);
 const arr = input[1].split(" ").map(el => Number(el));
 
-let answer = 1000;
+let answer = Infinity;
 for (let i = 0; i < n; i++) {
     let sum = 0;
     for (let j = 0; j < n; j++) {
