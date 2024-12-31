aba45bfc5d63224368e64c2d8ef95dcd80d5217a
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2025-01-01 00:56:07 +0900

    Title: 체크판위에서 2, Time: 185ms, Memory: 15MB, Status: Passed - CodeTree

--- 250101/체크판위에서 2/on-the-checkboard-2.js
+++ 250101/체크판위에서 2/on-the-checkboard-2.js
@@ -6,12 +6,13 @@ const arr = input.slice(1).map(el => el.split(" "));
 let answer = 0;
 const startColor = arr[0][0];
 const endColor = arr[n - 1][m - 1];
+
 if (startColor !== endColor) {
     for (let i = 1; i < n - 2; i++) {
         for (let j = 1; j < m - 2; j++) {
             if (arr[i][j] === endColor) {
                 for (let k = i + 1; k < n - 1; k++) {
-                    for (let l = j + 1; l < n - 1; l++) {
+                    for (let l = j + 1; l < m - 1; l++) {
                         if (arr[k][l] === startColor) {
                             answer++;
                         }
