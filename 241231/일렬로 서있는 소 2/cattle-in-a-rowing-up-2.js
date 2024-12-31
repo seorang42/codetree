ea3d87024d0115fa86ef1f6c439373f4e4c6f6cc
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-12-31 17:23:09 +0900

    Title: 일렬로 서있는 소 2, Time: 195ms, Memory: 15MB, Status: Passed - CodeTree

--- 241231/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.js
+++ 241231/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.js
@@ -6,9 +6,9 @@ const arr = input[1].split(" ").map(el => Number(el));
 let answer = 0;
 for (let i = 0; i < n - 2; i++) {
     for (let j = i + 1; j < n - 1; j++) {
-        if (arr[j] > arr[i]) {
+        if (arr[j] >= arr[i]) {
             for (let k = j + 1; k < n; k++) {
-                if (arr[k] > arr[j]) {
+                if (arr[k] >= arr[j]) {
                     answer++;
                 }
             }
