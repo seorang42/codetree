15d58be845941ca4e2c8e6b6a4651fd4717d431e
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-12-31 22:55:39 +0900

    Title: 오목, Time: 173ms, Memory: 14MB, Status: Runtime Error - CodeTree

---
+++ 241231/오목/O-mok.js
@@ -0,0 +1,56 @@
+const fs = require("fs");
+const board = fs.readFileSync(0).toString().trim().split("\n").map(el => el.split(" ").map(el => Number(el)));
+
+let [winner, location] = [0, []];
+for (let i = 0; i < 19; i++) {
+    for (let j = 0; j < 19; j++) {
+        // 가로로 같을 때
+        if (winner === 0 && board[i][j] === board[i][j + 1]) {
+            for (let k = j + 2; k < j + 5; k++) {
+                if (board[i][j] !== board[i][k]) {
+                    break;
+                }
+                if (k === j + 4 && board[i][j] === board[i][k]) {
+                    winner = board[i][j];
+                    location = [i + 1, j + 3];
+                }
+            }
+        }
+
+        // 세로로 같을 때
+        if (winner === 0 && board[i][j] === board[i + 1][j]) {
+            for (let k = i + 2; k < i + 5; k++) {
+                if (board[i][j] !== board[k][j]) {
+                    break;
+                }
+                if (k === i + 4 && board[i][j] === board[k][j]) {
+                    winner = board[i][j];
+                    location = [i + 3, j + 1];
+                }
+            }
+        }
+
+        // 대각선으로 같을 때
+        if (winner === 0 && board[i][j] === board[i + 1][j + 1]) {
+            for (let k = 2; k < 5; k++) {
+                if (board[i][j] !== board[i + k][j + k]) {
+                    break;
+                }
+                if (k === 4 && board[i][j] === board[i + k][j + k]) {
+                    winner = board[i][j];
+                    location = [i + 3, j + 3];
+                }
+            }
+        }
+
+        if (winner !== 0) {
+            break;
+        }
+    }
+    if (winner !== 0) {
+        break;
+    }
+}
+
+console.log(winner);
+console.log(location.join(" "));
