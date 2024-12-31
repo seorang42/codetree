77fd92f79a83cc65ae42e8fd8d5b4b35f113e067
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-12-31 16:55:45 +0900

    Title: 모이자, Time: 176ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241231/모이자/gather.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const n = Number(input[0]);
+const arr = input[1].split(" ").map(el => Number(el));
+
+let answer = 1000;
+for (let i = 0; i < n; i++) {
+    let sum = 0;
+    for (let j = 0; j < n; j++) {
+        if (i === j) {
+            continue;
+        }
+        sum += Math.abs(i - j) * arr[j];
+    }
+    if (sum < answer) {
+        answer = sum;
+    }
+}
+
+console.log(answer);
