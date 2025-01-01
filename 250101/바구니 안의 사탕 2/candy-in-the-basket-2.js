7eaa89a0de09ac6f1501ad66773af3c38f9f3722
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2025-01-01 19:34:56 +0900

    Title: 바구니 안의 사탕 2, Time: 160ms, Memory: 15MB, Status: Passed - CodeTree

--- 250101/바구니 안의 사탕 2/candy-in-the-basket-2.js
+++ 250101/바구니 안의 사탕 2/candy-in-the-basket-2.js
@@ -4,20 +4,24 @@ const [N, K] = input[0].split(" ").map(el => Number(el));
 const arr = input.slice(1).map(el => el.split(" ").map(el => Number(el))).sort((a, b) => a[1] - b[1]);
 const line = Array(101).fill(0);
 
-const [start, last] = [arr[0][1], arr[arr.length - 1][1]];
-
 arr.forEach(el => {
     const [count, location] = el;
     line[location] += count;
 });
 
 let answer = 0;
-for (let i = start + K; i <= last - K; i++) {
-    let sum = 0;
-    for (let j = i - K; j <= i + K; j++) {
-        sum += line[j];
+if (K >= 50) {
+    line.forEach(el => {
+        answer += el;
+    });
+} else {
+    for (let i = K; i <= 100 - K; i++) {
+        let sum = 0;
+        for (let j = i - K; j <= i + K; j++) {
+            sum += line[j];
+        }
+        if (sum > answer) answer = sum;
     }
-    if (sum > answer) answer = sum;
 }
 
 console.log(answer);
