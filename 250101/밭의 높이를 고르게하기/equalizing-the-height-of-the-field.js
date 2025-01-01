bfa0fdb53227a44b7fd2dfe738dba303eeaf1169
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2025-01-01 21:56:53 +0900

    Title: 밭의 높이를 고르게하기, Time: 172ms, Memory: 15MB, Status: Passed - CodeTree

--- 250101/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.js
+++ 250101/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.js
@@ -6,7 +6,7 @@ const arr = input[1].split(" ").map(el => Number(el));
 let answer = Infinity;
 for (let i = 0; i <= N - T; i++) {
     let sum = 0;
-    for (let j = i; j < i + 3; j++) {
+    for (let j = i; j < i + T; j++) {
         sum += Math.abs(H - arr[j]);
     }
     if (sum < answer) {
