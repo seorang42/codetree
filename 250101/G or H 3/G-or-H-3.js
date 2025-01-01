d1a5d86cc8cc1ee2c0f5d7c50ba4d330d6e0f853
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2025-01-01 17:04:27 +0900

    Title: G or H 3, Time: 173ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250101/G or H 3/G-or-H-3.js
+++ 250101/G or H 3/G-or-H-3.js
@@ -2,17 +2,24 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split("\n");
 const [N, K] = input[0].split(" ").map(el => Number(el));
 const arr = input.slice(1).map(el => el.split(" ")).sort((a, b) => a[0] - b[0]);
-const last = Number(arr[arr.length - 1][0]);
+const [first, last] = [Number(arr[0][0]), Number(arr[arr.length - 1][0])];
 
 let answer = 0;
-for (let i = 0; i < last - K; i++) {
+for (let i = first; i <= last - K + 1; i++) {
+    const [start, end] = [i, i + K];
     let sum = 0;
-    for (let j = 0; j < N; j++) {
-        if (Number(arr[j][0]) >= i + 1 && Number(arr[j][0]) <= i + K + 1) {
-            sum += arr[j][1] === "G" ? 1 : 2;
+    for (let i = 0; i < arr.length; i++) {
+        if (Number(arr[i][0]) < start) {
+            continue;
         }
+        if (Number(arr[i][0]) > end) {
+            break;
+        }
+        sum += arr[i][1] === "G" ? 1 : 2;
     }
-    if (sum > answer) answer = sum;
+    if (sum > answer) {
+        answer = sum;
+    }
 }
 
 console.log(answer);
