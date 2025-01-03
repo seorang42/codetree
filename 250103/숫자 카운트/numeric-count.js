b9c14f23605dd88e369f5d62d4dc6b2510c7084b
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2025-01-03 16:42:28 +0900

    Title: 숫자 카운트, Time: 183ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250103/숫자 카운트/numeric-count.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const N = Number(input[0]);
+const arr = input.slice(1).map(el => el.split(" ").map(el => Number(el)));
+
+const checkSB = (answer) => {
+    for (let i = 0; i < arr.length; i++) {
+        let [s, b] = [0, 0];
+        const answerArr = String(answer).split("").map(el => Number(el));
+        const inputArr = String(arr[i][0]).split("").map(el => Number(el));
+        inputArr.forEach(((el, index) => {
+            if (el === answerArr[index]) {
+                s++;
+            } else if (answerArr.includes(el)) {
+                b++;
+            }
+        }));
+        if (JSON.stringify([s, b]) !== JSON.stringify([arr[i][1], arr[i][2]])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+let count = 0;
+for (let i = 1; i <= 9; i++) {
+    for (let j = 1; j <= 9; j++) {
+        for (let k = 1; k <= 9; k++) {
+            const answer = i * 100 + j * 10 + k;
+            const isCorrect = checkSB(answer);
+            if (isCorrect) count++;
+        }
+    }
+}
+
+console.log(count);
