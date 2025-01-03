a4071f50c9dbbdbc3ec47467273af535b4da23c5
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2025-01-03 17:38:40 +0900

    Title: 숫자 카운트, Time: 184ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250103/숫자 카운트/numeric-count.js
+++ 250103/숫자 카운트/numeric-count.js
@@ -3,38 +3,32 @@ const input = fs.readFileSync(0).toString().trim().split("\n");
 const N = Number(input[0]);
 const arr = input.slice(1).map(el => el.split(" ").map(el => Number(el)));
 
-const checkSB = (answer) => {
-    for (let i = 0; i < arr.length; i++) {
-        let [s, b] = [0, 0];
-        const answerArr = String(answer).split("").map(el => Number(el));
-        const inputArr = String(arr[i][0]).split("").map(el => Number(el));
-        inputArr.forEach(((el, index) => {
-            if (el === answerArr[index]) {
-                s++;
-            } else {
-                let flag = false;
-                answerArr.forEach((e, i) => {
-                    if (i !== index && e === el) {
-                        flag = true;
-                    }
-                });
-                if (flag) b++;
-            }
-        }));
-        if (JSON.stringify([s, b]) !== JSON.stringify([arr[i][1], arr[i][2]])) {
-            return false;
-        }
-    }
-    return true;
-}
-
 let count = 0;
 for (let i = 1; i <= 9; i++) {
     for (let j = 1; j <= 9; j++) {
         for (let k = 1; k <= 9; k++) {
-            const answer = i * 100 + j * 10 + k;
-            const isCorrect = checkSB(answer);
-            if (isCorrect) count++;
+            let flag = true;
+            arr.forEach(el => {
+                let [S, B] = [0, 0];
+                const [input, s, b] = el;
+                const inputArr = String(input).split("").map(el => Number(el));
+                const answerArr = [i, j, k];
+                inputArr.forEach((el, index) => {
+                    if (el === answerArr[index]) {
+                        S++;
+                    } else {
+                        answerArr.forEach((e, i) => {
+                            if (index !== i && e === el) {
+                                B++;
+                            }
+                        });
+                    }
+                });
+                if (JSON.stringify([s, b]) !== JSON.stringify([S, B])) {
+                    flag = false;
+                }
+            });
+            if (flag) count++;
         }
     }
 }
