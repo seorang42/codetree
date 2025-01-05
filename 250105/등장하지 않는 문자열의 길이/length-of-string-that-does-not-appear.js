837bf67f8210fd042ae00753d3b84541fa480b55
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2025-01-05 22:39:03 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 215ms, Memory: 17MB, Status: Passed - CodeTree

--- 250105/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.js
+++ 250105/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.js
@@ -7,10 +7,21 @@ let answer = 0;
 for (let i = 1; i <= N; i++) {
     let flag = false;
     for (let j = 0; j <= N - i; j++) {
-        if (flag) break;
         const target = letter.slice(j, j + i);
-        const count = letter.split(target).length - 1;
-        if (count >= 2) flag = true;
+        let count = 0;
+        for (let k = j; k <= N - i; k++) {
+            let result = "";
+            for (let l = k; l < k + i; l++) {
+                result += letter[l];
+            }
+            if (result === target) {
+                count++;
+            }
+        }
+        if (count >= 2) {
+            flag = true;
+            break;
+        }
     }
     if (!flag) {
         answer = i;
