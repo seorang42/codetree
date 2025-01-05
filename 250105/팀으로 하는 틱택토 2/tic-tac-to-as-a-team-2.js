7ef2e6839b386f39df826db2799bb0c6f48d61d1
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2025-01-05 19:33:08 +0900

    Title: 팀으로 하는 틱택토 2, Time: 180ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250105/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.js
+++ 250105/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.js
@@ -5,17 +5,17 @@ let answer = 0;
 for (let i = 1; i <= 8; i++) {
     for (let j = i + 1; j <= 9; j++) {
         // 가로
-        if ([A, B, C].filter(el => [i, j].includes(el)).length === 3 || [D, E, F].filter(el => [i, j].includes(el)).length === 3 || [G, H, I].filter(el => [i, j].includes(el)).length === 3) {
+        if ([A, B, C].filter(el => el === i || el === j).length === 3 || [D, E, F].filter(el => el === i || el === j).length === 3 || [G, H, I].filter(el => el === i || el === j).length === 3) {
             answer++;
             continue;
         }
         // 세로
-        if ([A, D, G].filter(el => [i, j].includes(el)).length === 3 || [B, E, H].filter(el => [i, j].includes(el)).length === 3 || [C, F, I].filter(el => [i, j].includes(el)).length === 3) {
+        if ([A, D, G].filter(el => el === i || el === j).length === 3 || [B, E, H].filter(el => el === i || el === j).length === 3 || [C, F, I].filter(el => el === i || el === j).length === 3) {
             answer++;
             continue;
         }
         // 대각선
-        if ([A, E, I].filter(el => [i, j].includes(el)).length === 3 || [C, E, G].filter(el => [i, j].includes(el)).length === 3) {
+        if ([A, E, I].filter(el => el === i || el === j).length === 3 || [C, E, G].filter(el => el === i || el === j).length === 3) {
             answer++;
             continue;
         }
