59d4f317ac17cbdcea7ec6c4c09771d08b86d176
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2025-01-05 23:16:44 +0900

    Title: 팀으로 하는 틱택토 2, Time: 182ms, Memory: 15MB, Status: Passed - CodeTree

--- 250105/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.js
+++ 250105/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.js
@@ -4,14 +4,14 @@ const [[A, B, C], [D, E, F], [G, H, I]] = fs.readFileSync(0).toString().split("\
 let answer = 0;
 for (let i = 1; i <= 8; i++) {
     for (let j = i + 1; j <= 9; j++) {
-        if ([A, B, C].filter(el => el === i).length + [A, B, C].filter(el => el === j).length === 3 || 
-        [D, E, F].filter(el => el === i).length + [D, E, F].filter(el => el === j).length === 3 || 
-        [G, H, I].filter(el => el === i).length + [G, H, I].filter(el => el === j).length === 3 || 
-        [A, D, G].filter(el => el === i).length + [A, D, G].filter(el => el === j).length === 3 || 
-        [B, E, H].filter(el => el === i).length + [B, E, H].filter(el => el === j).length === 3 || 
-        [C, F, I].filter(el => el === i).length + [C, F, I].filter(el => el === j).length === 3 || 
-        [A, E, I].filter(el => el === i).length + [A, E, I].filter(el => el === j).length === 3 || 
-        [C, E, G].filter(el => el === i).length + [C, E, G].filter(el => el === j).length === 3) {
+        if ([A, B, C].filter(el => el === i).length + [A, B, C].filter(el => el === j).length === 3 && [A, B, C].filter(el => el === i).length !== 3 && [A, B, C].filter(el => el === j).length !== 3 || 
+        [D, E, F].filter(el => el === i).length + [D, E, F].filter(el => el === j).length === 3 && [D, E, F].filter(el => el === i).length !== 3 && [D, E, F].filter(el => el === j).length !== 3 || 
+        [G, H, I].filter(el => el === i).length + [G, H, I].filter(el => el === j).length === 3 && [G, H, I].filter(el => el === i).length !== 3 && [G, H, I].filter(el => el === j).length !== 3 || 
+        [A, D, G].filter(el => el === i).length + [A, D, G].filter(el => el === j).length === 3 && [A, D, G].filter(el => el === i).length !== 3 && [A, D, G].filter(el => el === j).length !== 3 || 
+        [B, E, H].filter(el => el === i).length + [B, E, H].filter(el => el === j).length === 3 && [B, E, H].filter(el => el === i).length !== 3 && [B, E, H].filter(el => el === j).length !== 3 || 
+        [C, F, I].filter(el => el === i).length + [C, F, I].filter(el => el === j).length === 3 && [C, F, I].filter(el => el === i).length !== 3 && [C, F, I].filter(el => el === j).length !== 3 || 
+        [A, E, I].filter(el => el === i).length + [A, E, I].filter(el => el === j).length === 3 && [A, E, I].filter(el => el === i).length !== 3 && [A, E, I].filter(el => el === j).length !== 3 || 
+        [C, E, G].filter(el => el === i).length + [C, E, G].filter(el => el === j).length === 3 && [C, E, G].filter(el => el === i).length !== 3 && [C, E, G].filter(el => el === j).length !== 3 ) {
             answer++;
         }
     }
