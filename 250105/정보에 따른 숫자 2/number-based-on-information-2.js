5598d5334a3cf39959d4b43cbcc500d2700cc848
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2025-01-05 17:26:55 +0900

    Title: 정보에 따른 숫자 2, Time: 193ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250105/정보에 따른 숫자 2/number-based-on-information-2.js
+++ 250105/정보에 따른 숫자 2/number-based-on-information-2.js
@@ -2,22 +2,21 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split("\n");
 const [T, a, b] = input[0].split(" ").map(el => Number(el));
 const arr = input.slice(1).map(el => el.split(" "));
+const line = Array(1000).fill("");
 
-const OFFSET = a;
-const line = Array(b - a + 1).fill("");
-for (let i = 0; i < T; i++) {
-    line[Number(arr[i][1]) - OFFSET] = arr[i][0];
-}
+arr.forEach(el => {
+    line[Number(el[1]) - 1] = el[0];
+});
 
 let answer = 0;
-for (let i = 0; i < line.length; i++) {
+for (let i = a; i <= b; i++) {
     let [sDis, nDis] = [Infinity, Infinity];
-    for (let j = 0; j < line.length; j++) {
+    for (let j = 0; j <= line.length; j++) {
         if (line[j] === "S" && Math.abs(j - i) < sDis) {
             sDis = Math.abs(j - i);
         }
     }
-    for (let j = 0; j < line.length; j++) {
+    for (let j = 0; j <= line.length; j++) {
         if (line[j] === "N" && Math.abs(j - i) < nDis) {
             nDis = Math.abs(j - i);
         }
