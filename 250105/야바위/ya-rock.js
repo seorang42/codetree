1cfdc2db8d6d1760dd37f9ab25171f65336c1d3b
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2025-01-05 18:26:06 +0900

    Title: 야바위, Time: 210ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250105/야바위/ya-rock.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const N = Number(input[0]);
+const arr = input.slice(1).map(el => el.split(" ").map(el => Number(el)));
+
+let [max, answer] = [0, 0];
+for (let i = 1; i <= 3; i++) {
+    let score = 0;
+    let location = i;
+    for (let j = 0; j < N; j++) {
+        if (arr[j][0] === location) {
+            location = arr[j][1];
+        } else if (arr[j][1] === location) {
+            location = arr[j][0];
+        }
+        if (location === arr[j][2]) score++;
+    }
+    if (score > max) {
+        max = score;
+        answer = i;
+    }
+}
+
+console.log(answer);
