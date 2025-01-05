5af307e9e260c275e3b0e69749a34ef44f50cea6
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2025-01-05 23:11:39 +0900

    Title: 팀으로 하는 틱택토 2, Time: 169ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250105/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.js
+++ 250105/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.js
@@ -4,20 +4,15 @@ const [[A, B, C], [D, E, F], [G, H, I]] = fs.readFileSync(0).toString().split("\
 let answer = 0;
 for (let i = 1; i <= 8; i++) {
     for (let j = i + 1; j <= 9; j++) {
-        // 가로
-        if ([A, B, C].filter(el => el === i || el === j).length === 3 || [D, E, F].filter(el => el === i || el === j).length === 3 || [G, H, I].filter(el => el === i || el === j).length === 3) {
+        if ([A, B, C].filter(el => el === i).length + [A, B, C].filter(el => el === j).length === 3 || 
+        [D, E, F].filter(el => el === i).length + [D, E, F].filter(el => el === j).length === 3 || 
+        [G, H, I].filter(el => el === i).length + [G, H, I].filter(el => el === j).length === 3 || 
+        [A, D, G].filter(el => el === i).length + [A, D, G].filter(el => el === j).length === 3 || 
+        [B, E, H].filter(el => el === i).length + [B, E, H].filter(el => el === j).length === 3 || 
+        [C, F, I].filter(el => el === i).length + [C, F, I].filter(el => el === j).length === 3 || 
+        [A, E, I].filter(el => el === i).length + [A, E, I].filter(el => el === j).length === 3 || 
+        [C, E, G].filter(el => el === i).length + [C, E, G].filter(el => el === j).length === 3) {
             answer++;
-            continue;
-        }
-        // 세로
-        if ([A, D, G].filter(el => el === i || el === j).length === 3 || [B, E, H].filter(el => el === i || el === j).length === 3 || [C, F, I].filter(el => el === i || el === j).length === 3) {
-            answer++;
-            continue;
-        }
-        // 대각선
-        if ([A, E, I].filter(el => el === i || el === j).length === 3 || [C, E, G].filter(el => el === i || el === j).length === 3) {
-            answer++;
-            continue;
         }
     }
 }
