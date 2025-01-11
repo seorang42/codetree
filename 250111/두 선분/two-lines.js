ea07ac4cb580f682f49980c26534b203c897d8a3
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2025-01-11 21:43:05 +0900

    Title: 두 선분, Time: 170ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250111/두 선분/two-lines.js
+++ 250111/두 선분/two-lines.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const [x1, x2, x3, x4] = fs.readFileSync(0).toString().trim().split(" ").map(el => Number(el));
 
-if (x2 >= x3 || x1 >= x4 || (x1 <= x3 && x2 <= x4) || (x3 <= x1 && x4 <= x2)) {
+if (x2 >= x3 || x1 >= x4 || (x1 <= x3 && x4 <= x2) || (x3 <= x1 && x2 <= x4)) {
     console.log("intersecting");
 } else {
     console.log("nonintersecting");
